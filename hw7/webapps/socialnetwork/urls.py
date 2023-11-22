52ca81d08b47730e9c8d4acf8a51c523e43aebb9
Kathy Chiang <51814166+Kathyccc@users.noreply.github.com>
2023-11-21 21:30:14 -0500

    Delete hw7/webapps directory

--- hw7/webapps/socialnetwork/urls.py
+++
@@ -1,9 +0,0 @@
-# from socialnetwork import views
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('', views.global_stream_action, name='home'),
-#     path('get-global', views.get_global, name='get-global'),
-#     path('get-follower', views.get_follower, name='get-follower'),
-#     path('add-comment', views.add_comment, name='add-comment'),
-# ]
